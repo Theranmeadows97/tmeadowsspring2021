1f9fc95f0e8c745af0c70c3ec28506fb6b08b5d0
Theranmeadows97 <theranmeadows@gmail.com>
2021-04-21 13:15:03 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -11,6 +11,61 @@ const links = [
         label: "Week3",
         url: "week3/index.html"
     }
+    ,
+    {
+        label: "Week4",
+        url: "week4/index.html"
+    }
+    ,
+    {
+        label: "Week5",
+        url: "week5/index.html"
+    }
+    ,
+    {
+        label: "Week6",
+        url: "week6/index.html"
+    }
+    ,
+    {
+        label: "Week7",
+        url: "week7/index.html"
+    }
+    ,
+    {
+        label: "Week8",
+        url: "week8/index.html"
+    }
+    ,
+    {
+        label: "Week9",
+        url: "week9/index.html"
+    }
+    ,
+    {
+        label: "Week10",
+        url: "week10/index.html"
+    }
+    ,
+    {
+        label: "Week11",
+        url: "week11/index.html"
+    }
+    ,
+    {
+        label: "Week12",
+        url: "week12/index.html"
+    }
+    ,
+    {
+        label: "Week13",
+        url: "week13/index.html"
+    }
+    ,
+    {
+        label: "Week14",
+        url: "week14/index.html"
+    }
   ]
 
   var output = '';
